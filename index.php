6f665850c5aaaff829135cc739689b658d4d2987
Carlos Fiori <carlosfiori@gmail.com>
2017-05-20 14:09:52 -0300

    refatoracao

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
-echo "<pre>";
-ini_set('display_errors', 1);
 
-require_once "./Source/SenhaIncorretaException.php";
-require_once "./Source/Senha.php";
-require_once "./Source/CoisaSeguraComAbrirEFechar.php";
-require_once "./Source/ValidadorDeSenha.php";
-require_once "./Source/Cofre.php";
-require_once "./Source/Senha.php";
-require_once "./Source/Maleta.php";
-require_once "./Source/ValidadorDeSenhaMaiorQue.php";
-require_once "./Source/ValidadorDeSenhaMaiorQueAlfaNumerica.php";
-require_once "./Source/ValidadorDeSenhaMaiorQueAlfaNumericaMaiusculo.php";
-require_once "./Source/ValidadorDeSenhaComposite.php";
-require_once "./Source/ValidadorDeSenhaTemLetras.php";
-require_once "./Source/ValidadorDeSenhaTemNumero.php";
-require_once "./Source/ValidadorDeSenhaSemValidacao.php";
-require_once "./Source/CofreFactory.php";
-require_once "./Source/CofreDigitalV2.php";
-require_once "./Source/CofreAnalogico.php";
-
-
-$cofreSemValidacao = CofreFactory::criarCofreSemValidacao('CofreDigital', 'aaaaa');
-$cofreSemValidacao->abrir('aaaaa');
-$cofreComValidacao = CofreFactory::criarCofreComValidacao('CofreAnalogico','qwe1', new ValidadorDeSenhaTemNumero());
-$cofreComValidacao->abrir('qwe1');
-
+require_once "index_cofre.php";
---
+++ index_cofre.php
@@ -0,0 +1,28 @@
+<?php
+echo "<pre>";
+ini_set('display_errors', 1);
+
+require_once "./Source/Cofre/SenhaIncorretaException.php";
+require_once "./Source/Cofre/Senha.php";
+require_once "./Source/Cofre/CoisaSeguraComAbrirEFechar.php";
+require_once "./Source/Cofre/ValidadorDeSenha.php";
+require_once "./Source/Cofre/Cofre.php";
+require_once "./Source/Cofre/Senha.php";
+require_once "./Source/Cofre/Maleta.php";
+require_once "./Source/Cofre/ValidadorDeSenhaMaiorQue.php";
+require_once "./Source/Cofre/ValidadorDeSenhaMaiorQueAlfaNumerica.php";
+require_once "./Source/Cofre/ValidadorDeSenhaMaiorQueAlfaNumericaMaiusculo.php";
+require_once "./Source/Cofre/ValidadorDeSenhaComposite.php";
+require_once "./Source/Cofre/ValidadorDeSenhaTemLetras.php";
+require_once "./Source/Cofre/ValidadorDeSenhaTemNumero.php";
+require_once "./Source/Cofre/ValidadorDeSenhaSemValidacao.php";
+require_once "./Source/Cofre/CofreFactory.php";
+require_once "./Source/Cofre/CofreDigitalV2.php";
+require_once "./Source/Cofre/CofreAnalogico.php";
+
+
+$cofreSemValidacao = CofreFactory::criarCofreSemValidacao('CofreDigital', 'aaaaa');
+$cofreSemValidacao->abrir('aaaaa');
+$cofreComValidacao = CofreFactory::criarCofreComValidacao('CofreAnalogico','qwe1', new ValidadorDeSenhaTemNumero());
+$cofreComValidacao->abrir('qwe1');
+
